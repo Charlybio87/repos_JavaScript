93361efc27d00ac9662d018d9d3bfcac44ff5e86
Carlos Alberto Ribas <ribascarlos.a@outlook.com>
2023-09-29 15:44:07 -0300

    new carpeta Visual

---
+++ Visual/MiMO/script.js
@@ -0,0 +1,14 @@
+console.log("hola mundo, como estas!!??");
+const dessert = 'Cookies';
+console.log(dessert);
+console.log(5!==5);
+if (true) {
+    console.log('Hello');
+}
+if (dessert!=='state') {
+    let suma = 5+5;
+    console.log(suma);
+}
+let likes = 5;
+likes += 1;
+console.log(likes)
---
+++ Visual/Prueba v1.0/prueba.js
@@ -0,0 +1,27 @@
+/** ---Comentario de Documentacion---
+ * CaC Inicial
+ * Programa: Pruebas 
+ * @author: CISCO Curso de JavaScript
+ * version 1.0
+ * @see: https://acortar.link/HfAr2c 
+ */
+
+/* ---Comentario de bloque--- (este coment. no se visualiza en documentacion)
+* En este espacio de trabajo se realizo prueba de impresion de hola mundo
+* como tambien de definicion de algunas variables y su impresion.
+* Para saber los atajos de
+*/
+
+// ---Comentario de Linea---
+// INICIO DE ALGORITMO
+
+let nota_final = 10
+
+if (nota_final >= 6){
+    console.log("Aprobo la asignatura")
+    console.log("Puedes descansar")
+}
+else{
+    console.log("Reprobo la asignatura")
+    console.log("Sigue estudiando")
+}
